4a6e8379561a786f35ede1b83ad3ff3a496c937a
Sarah Schaller <sarahcschaller@gmail.com>
2018-09-06 19:09:23 +0000

    automatically backed up by learn

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,19 +1,29 @@
 class ProductsController < ApplicationController
-  def index 
-    @products = Product.all 
-  end 
+  def index
+    @products = Product.all
+  end
 
-  def new 
-    @product = Product.new 
-  end 
+  def new
+    @product = Product.new
+  end
 
-  def create 
+  def create
     @product = Product.create(product_params)
-    redirect_to products_path 
+    redirect_to products_path
+  end
+
+  def inventory 
+    product = Product.find(params[:id])
+    render plain: product.inventory > 0 ? true : false 
   end 
 
-  private 
-  def product_params 
+  def description 
+    product = Product.find(params[:id])
+    render plain: product.description 
+  end
+
+  private
+  def product_params
     params.require(:product).permit(:name, :description, :inventory, :price)
   end 
 end
